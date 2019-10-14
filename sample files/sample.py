8339f6d3310f7138479fcc6967ff3e1786ede04d
froido <froido@gmx.de>
2019-10-14 12:58:02 +0200

    added use of get_random_dataset()

--- sample files/sample.py
+++ sample files/sample.py
@@ -1,5 +1,6 @@
 import numpy
 import mesh_transition as mt
+import random_mesh_input as rand
 
 # Load Abaqus and Pace3D Files into arrays
 abaqus_data = mt.read_abaqus('abaqus_voidRatio.csv', '')
@@ -8,6 +9,8 @@ pace3d_data = mt.read_pace3d('pace3d_pressure_2D_sample.dat', '')
 mesh_in = numpy.array([abaqus_data[0], abaqus_data[1]])
 mesh_out = numpy.array([pace3d_data[0], pace3d_data[1]])
 data_in = abaqus_data[3]
+
+rand.get_random_dataset(data_in, -0.1, True)
 
 # Transferring data from Abaqus mesh to Pace3d grid
 data = mt.mesh_transformation(mesh_in, mesh_out, data_in)
