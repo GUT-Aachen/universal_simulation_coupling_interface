748db3ab5c1e80215b6cda78762dc808b0a58428
froido <froido@gmx.de>
2020-04-09 15:08:38 +0200

    * read_part_nodes: Output is set to be allways [x,y,z,node]

--- abaqus_input.py
+++ abaqus_input.py
@@ -404,7 +404,6 @@ def read_part_nodes (input_file_path, part_name):
 
     Returns:
         ndarray: Containing nodes and corresponding coordinates for each node in part_name [x, y, z, node]
-                    or [x, y, node]
     """
 
     function_name = 'read_part_nodes'
@@ -496,10 +495,10 @@ def read_part_nodes (input_file_path, part_name):
         # Return ndarray containing a set of nodes and the corresponding coordinates.
         if z_array.__len__() == 0:
             log.info('Added %s nodes with x/y-coordinates', node_array.__len__())
-            return numpy.array([x_array, y_array, node_array])
         else:
             log.info('Added %s nodes with x/y/z-coordinates', node_array.__len__())
-            return numpy.array([x_array, y_array, z_array, node_array])
+
+        return numpy.array([x_array, y_array, z_array, node_array])
 
     except Exception as err:
         log.error(str(err))
