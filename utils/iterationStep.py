61c80696716df33c8d88def4532d99da6d628577
froido <froido@gmx.de>
2020-06-09 14:57:48 +0200

    added docstring

--- utils/iterationStep.py
+++ utils/iterationStep.py
@@ -4,12 +4,31 @@ from pathlib import Path
 
 
 class IterationsDict(dict):
+    """
+    IterationsDict is a modified Dictionary object (dict). It has the additional option to add a new iteration step, by
+    setting a iterations name and number.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.log = log.getLogger(self.__class__.__name__)
 
     def add_iteration_step(self, iteration_name, step_no=None):
+        """
+        Adding an additional step into this instance. This step will be added into the dictionary and a IterationStep
+        object is initialized. A name for this step (iterations_name) is mandatory and a step number (step_no) can be
+        added. If not set, the step number (step_no) will be increased by one/set to the length of the instance.
+        The new entry in the instance (dictionary) is stored with iteration_name as key and IterationStep object as
+        value.
+
+        Args:
+            iteration_name (str): Name of the added step.
+            step_no (int, optional): Number of the added step. Default:None
+
+        Returns:
+            Added entry of the instance as a dict value.
+
+        """
         if iteration_name in self:
             raise NameError(f'Step with the same name ({iteration_name}) already exists.')
         else:
@@ -22,9 +41,27 @@ class IterationsDict(dict):
 
 
 class IterationStep:
-    def __init__(self, iteration_name, step_no):
+    """
+    An IterationStep object combines all information needed to run or collected from a finished simulation step. Those
+    are for example a Grid object, file Path object and addition information like name or computing time. These
+    IterationStep objects are usually stored in an IterationsDict object.
+    """
 
-        self.grid = Grid()
+    def __init__(self, iteration_name: str, step_no: int):
+        """
+
+        Args:
+            iteration_name (str): Name of the iteration
+            step_no (int): Number of the step
+        """
+
+        # Check input parameters
+        if not isinstance(iteration_name, str):
+            raise TypeError(f'Input parameter iteration_name must be of type string.')
+        if not isinstance(step_no, int):
+            raise TypeError(f'Input parameter step_no must be of type integer.')
+
+        self.grid = Grid()  # Initialize Grid object
         self.name = iteration_name
         self.prefix = None
         self.computing_time = None
@@ -47,6 +84,10 @@ class IterationStep:
     __repr__ = __str__
 
     def set_prefix(self, prefix: str):
+        # Check input parameters
+        if not isinstance(prefix, str):
+            raise TypeError(f'Input parameter prefix must be of type string.')
+
         self.prefix = prefix
 
     def get_prefix(self):
@@ -54,6 +95,8 @@ class IterationStep:
 
     def set_step_folder(self, parent_folder):
         """
+        FUNCTION IS DEPRECATED Use create_step_folder() instead.
+
         Set sub folder where additional files for this step are stored.
 
         Args:
@@ -62,37 +105,39 @@ class IterationStep:
         Returns:
             boolean: true on success
         """
-        parent_folder = Path(parent_folder)
+        self.log.warning(f'This function (set_step_folder()) is deprecated. Use create_step_folder() instead.')
+        return self.create_step_folder(parent_folder, False)
 
-        sub_folder = f'step_{self.name}'
-
-        path = parent_folder / sub_folder
-
-        self.path = path
-
-        if not path.is_dir():
-            self.log.warning(f'Subfolder does not exist. {path}')
-            return path
-
-    def create_step_folder(self, parent_folder):
+    def create_step_folder(self, parent_folder, create_if_missing=True):
         """
-        Creating subfolder where additional files for this step are stored.
+        Setting and optionally creating sub folder where additional files for this step are stored.
 
         Args:
             parent_folder (str, Path): Path or string of path of the parent folder
+            create_if_missing (bool, optional): Shall the folder created if it does not exist?
 
         Returns:
             boolean: true on success
         """
-        parent_folder = Path(parent_folder)
+
+        # Check input parameters
+        if not isinstance(parent_folder, str) and not isinstance(parent_folder, Path):
+            raise TypeError(f'Input parameter parent_folder must be of type string or Path.')
+        else:
+            parent_folder = Path(parent_folder)
 
         sub_folder = f'step_{self.name}'
 
         path = parent_folder / sub_folder
 
         if not path.is_dir():
-            path.mkdir()
-            self.path = path
-            return path
-        if not path.is_dir():
-            raise FileNotFoundError(f'Subfolder does not exist and can not be created. {path}')
+            if create_if_missing:
+                path.mkdir()
+                self.log.debug(f'Sub folder set and created successfully to. ({path})')
+            else:
+                raise FileNotFoundError(f'Sub folder does not exist and shall not be created. ({path})')
+        else:
+            self.log.debug(f'Sub folder set successfully, but already exists. ({path})')
+
+        self.path = path
+        return path
