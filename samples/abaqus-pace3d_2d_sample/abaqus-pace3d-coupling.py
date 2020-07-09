e59859e4037fedabc234f7f37eae33f18b2218d6
froido <froido@gmx.de>
2020-07-09 09:49:04 +0200

    minor changes in execution sequence

--- samples/abaqus-pace3d_2d_sample/abaqus-pace3d-coupling.py
+++ samples/abaqus-pace3d_2d_sample/abaqus-pace3d-coupling.py
@@ -29,6 +29,13 @@ consoleLogger.setFormatter(format_logger)
 logging.getLogger('').addHandler(consoleLogger)  # Add handler to root logger
 log = logging.getLogger('sample' + '.main')  # Define a logger for __main__ file
 # #################################################################################
+# Before starting the simulation some additional parameters have to be set
+# simulation_name is the name of the simulation used as pre_name for exported files
+# number_of_steps sets the number of iterations steps, that shall be done by the following for-loop
+# As a boundary condition in the Simulia Abaqus simulation has to be affected by a previous simulation the part(s) and
+# assembly(ies) has(have) to be defines(d) by name.
+# abaqus_initial_pore_pressure_value is the initial pore pressure for the part named in abaqus_part_name/
+# abaqus_assembly_name.
 
 # Set simulation name
 simulation_name = 'AbaqusPace3dCoupling'
@@ -46,11 +53,12 @@ sim.output_path_cleanup()  # Clean up process for output folder
 abaqus_handler = sim.add_engine('abaqus')
 pace3d_handler = sim.add_engine('pace3d')
 
-# Tell the Abaqus engine where paths and files to be found. The scratch folder is used by Abaqus to store temporarily
-# needed files on the disk.
+# #################################################################################
+# Initializing Abaqus engine object
+
+# Tell the Abaqus engine where paths and files to be found.
 # Input: Contains all files that are needed to run the simulation
 # Output: Is the root folder for the output. In here all generated files will be stored.
-# Scratch: Abaqus can use a scratch folder so store temporary files
 abaqus_handler.set_path('input', sim.get_input_path())
 abaqus_handler.set_path('output', sim.get_output_path() / 'abaqus')
 
@@ -60,22 +68,28 @@ abaqus_subroutine_file_name = 'subroutine_export.f'
 abaqus_handler.set_file('subroutine', abaqus_handler.get_path('input') / abaqus_subroutine_file_name)
 
 # Set Abaqus input file to Abaqus engine
-abaqus_handler.set_file('input_file', abaqus_handler.get_path('input') / 'abaqus_coupling.inp')
+abaqus_input_file_name = 'abaqus_coupling.inp'
+abaqus_handler.set_file('input_file', abaqus_handler.get_path('input') / abaqus_input_file_name)
 
 # Initialize the Simulia Abaqus and Pace3D engine. Any engine needs a dictionary of input parameters. In case of
 # Abaqus only one key is needed: input_file
 abaqus_handler.init_engine({'input_file': abaqus_handler.get_file('input_file')})
+
+# #################################################################################
+# Initializing Pace3D engine object
+
+# Store Pace3D paths and files in pace3d_handler
 pace3d_handler.init_engine()
 
 # Set Pace3D paths and files
 pace3d_handler.set_path('input', sim.get_input_path())
 pace3d_handler.set_path('output', sim.get_output_path() / 'pace3d')
 # Name of Pace3D pore pressure file (csv) stored in ./input/
-pace3d_pore_pressure_file_name = 'pace3D_pore-pressure.dat'
-pace3d_handler.set_file('pore_pressure', pace3d_handler.get_path('input') / pace3d_pore_pressure_file_name)
+pace3d_pore_pressure_file_name = 'pace3D_pore-pressure_01.dat'
+pace3d_handler.set_file('pore_pressure_01', pace3d_handler.get_path('input') / pace3d_pore_pressure_file_name)
 # Name of Pace3D pore pressure file (csv) stored in ./input/
 pace3d_pore_pressure_initial_file_name = 'pace3D_pore-pressure_initial.dat'
-pace3d_handler.set_file('initial_pore_pressure', pace3d_handler.get_path('input') /
+pace3d_handler.set_file('pore_pressure_initial', pace3d_handler.get_path('input') /
                         pace3d_pore_pressure_initial_file_name)
 
 # Set the number of steps to be done
@@ -91,10 +105,11 @@ abaqus_part_name = 'Part-1'
 log.debug(f'root_directory: {sim.get_root_path()}')
 log.debug(f'simulation_handler_name: {sim.name}')
 log.debug(f'abaqus_subroutine_file_name: {abaqus_handler.get_file("subroutine")}')
-log.debug(f'pace3d_pore_pressure_file_name: {pace3d_handler.get_file("pore_pressure")}')
-log.debug(f'pace3d_pore_pressure_initial_file_name: {pace3d_handler.get_file("initial_pore_pressure")}')
+log.debug(f'pace3d_pore_pressure_file_name: {pace3d_handler.get_file("pore_pressure_01")}')
+log.debug(f'pace3d_pore_pressure_initial_file_name: {pace3d_handler.get_file("pore_pressure_initial")}')
 log.info(f'number_of_iterations: {number_of_steps}')
 
+# #################################################################################
 # #################################################################################
 # Preparing first step (geostatic)
 log.info('Preparing first step')
@@ -107,6 +122,25 @@ step_name = 'initial'
 # created step will be linked to a variable called actual_step. The step can alternatively called by
 # sim.get_current_iterations().
 actual_step = sim.add_iteration_step(step_name)
+
+
+# #################################################################################
+# Initialize Pace3D grid objects
+# Transfer Pace3d simulation results into Grid object
+
+data = pace3d_handler.engine.read_csv_file(file=pace3d_handler.get_file('pore_pressure_initial'),
+                                           x_coord_row=0, y_coord_row=1, z_coord_row=2,
+                                           values_row={'pore_pressure': 3})
+
+# Pace3D z coordinate in 2d is always 1.0 instead of 0. Setting z coordinate to 0 instead.
+for row in data:
+    row['z_coordinate'] = 0
+
+actual_step['pace3d'].grid.initiate_grid(data, 'pore_pressure')
+
+
+# #################################################################################
+# Preprosessing Simulia Abaqus engine
 actual_step['abaqus'].create_step_folder(abaqus_handler.get_path('output'))
 
 # The initial grid/mesh will be read from the Abaqus input file and stored in a grid object in the actual step. This
@@ -125,15 +159,6 @@ abaqus_handler.engine.create_node_set_all_list('PP', abaqus_part_name)
 
 # Set initial pore pressure distribution imported from an data file from Pace3D.
 log.debug(f'Setting initial pore pressure distribution by pace3d distribution data.')
-data = pace3d_handler.engine.read_csv_file(file=pace3d_handler.get_file('initial_pore_pressure'),
-                                           x_coord_row=0, y_coord_row=1, z_coord_row=2,
-                                           values_row={'pore_pressure': 3})
-
-# Pace3D z coordinate in 2d is always 1.0 instead of 0. Setting z coordinate to 0 instead.
-for row in data:
-    row['z_coordinate'] = 0
-
-actual_step['pace3d'].grid.initiate_grid(data, 'pore_pressure')
 
 # Transform data from Pace3D grid to Simulia Abaqus Mesh
 transformer = GridTransformer()
@@ -166,7 +191,8 @@ abaqus_handler.set_file(f'bash_file_{step_name}',
                             additional_parameters='cpus=2 interactive'
                         ))
 
-# Start the simulation by calling a sub process
+# #################################################################################
+# Start Abaqus the simulation by calling a sub process
 sim.call_subprocess(abaqus_handler.get_file(f'bash_file_{step_name}'), actual_step['abaqus'].path)
 
 # #################################################################################
@@ -192,29 +218,13 @@ transformer.add_grid(pore_pressure_import_grid, 'import')
 transformer.find_nearest_neighbors('import', 'abaqus', 4)
 transformer.transition('import', 'void_ratio', 'abaqus')
 
-# #################################################################################
-# PACE 3D STUFF
-# Prepare Transfer
-# Transfer Abaqus void ratio results into Pace3D Grid object
-transformer = GridTransformer()
-transformer.add_grid(actual_step['abaqus'].grid, 'abaqus')
-transformer.add_grid(actual_step['pace3d'].grid, 'pace3d')
-transformer.find_nearest_neighbors('abaqus', 'pace3d', 4)
-transformer.transition('abaqus', 'void_ratio', 'pace3d')
+# Transform void ratio to porosity
+data_dict = actual_step['abaqus'].grid.get_node_values('void_ratio')
 
-# Transfer from grid to Pace3D
-# Start Pace3D Simulation
+for key, item in data_dict.items():
+    data_dict[key] = item / (1 + item)
 
-# Transfer Pace3d simulation results into Grid object
-data = pace3d_handler.engine.read_csv_file(file=pace3d_handler.get_file('pore_pressure'),
-                                           x_coord_row=0, y_coord_row=1, z_coord_row=2,
-                                           values_row={'pore_pressure': 3})
-
-# Pace3D z coordinate in 2d is always 1.0 instead of 0. Setting z coordinate to 0 instead.
-for row in data:
-    row['z_coordinate'] = 0
-
-actual_step['pace3d'].grid.initiate_grid(data, 'pore_pressure')
+actual_step['abaqus'].grid.set_node_values('porosity', data_dict)
 
 # #################################################################################
 # Next Iteration
@@ -227,6 +237,41 @@ for x in range(0, number_of_steps):
     previous_step = sim.get_previous_iterations()  # for quick access
 
     # #################################################################################
+    # PACE 3D STUFF
+
+    # Change input file name every iteration. This is just a work around as the CLI implementation of Pace3D is not
+    # completed yet.
+    pace3d_file_next = f'pore_pressure_0{x + 1}'
+
+    # Prepare Transfer
+    # Transfer Abaqus pore pressure results into Pace3D Grid object
+    transformer = GridTransformer()
+    transformer.add_grid(actual_step['abaqus'].grid, 'abaqus')
+    transformer.add_grid(actual_step['pace3d'].grid, 'pace3d')
+    transformer.find_nearest_neighbors('abaqus', 'pace3d', 2)
+    transformer.transition('abaqus', 'porosity', 'pace3d')
+
+    data = actual_step['pace3d'].grid.get_list()
+    pace3d_handler.engine.write_csv_file(data, actual_step['abaqus'].get_path() / 'porosity.dat')
+
+    # #################################################################################
+    # Transfer from grid to Pace3D
+    # Start Pace3D Simulation
+    # #################################################################################
+
+    # Transfer Pace3d simulation results into Grid object
+    data = pace3d_handler.engine.read_csv_file(file=pace3d_handler.get_file(pace3d_file_next),
+                                               x_coord_row=0, y_coord_row=1, z_coord_row=2,
+                                               values_row={'pore_pressure': 3})
+    # Z-dimension in Pace3D is negative in Abaqus positive. Manipulate z-coordinates by multiplying by -1. Pore
+    # pressure is given in bar should be N/m²: multiply by 100000.
+    for row in data:
+        row['z_coordinate'] = 0
+
+    actual_step['pace3d'].grid.initiate_grid(data, 'pore_pressure')
+
+    # #################################################################################
+    # #################################################################################
     # SIMULIA ABAQUS STUFF
     # Preparing simulation iteration
     actual_step['abaqus'].create_step_folder(abaqus_handler.get_path('output'))
@@ -236,7 +281,7 @@ for x in range(0, number_of_steps):
     transformer.add_grid(actual_step['abaqus'].grid, 'abaqus')
     transformer.add_grid(actual_step['pace3d'].grid, 'pace3d')
 
-    transformer.find_nearest_neighbors('pace3d', 'abaqus', 4)
+    transformer.find_nearest_neighbors('pace3d', 'abaqus', 2)
     transformer.transition('pace3d', 'pore_pressure', 'abaqus')
 
     # Create modified boundary conditions in Abaqus input file. This must be done according to Abaqus manual
@@ -304,16 +349,10 @@ for x in range(0, number_of_steps):
     transformer.find_nearest_neighbors('import', 'abaqus', 4)
     transformer.transition('import', 'void_ratio', 'abaqus')
 
-    # #################################################################################
-    # PACE 3D STUFF
-    # Prepare Transfer
-    # Transfer Abaqus pore pressure results into Pace3D Grid object
-    transformer = GridTransformer()
-    transformer.add_grid(actual_step['abaqus'].grid, 'abaqus')
-    transformer.add_grid(actual_step['pace3d'].grid, 'pace3d')
-    transformer.find_nearest_neighbors('abaqus', 'pace3d', 4)
-    transformer.transition('abaqus', 'void_ratio', 'pace3d')
+    # Transform void ratio to porosity
+    data_dict = actual_step['abaqus'].grid.get_node_values('void_ratio')
 
-    # Transfer from grid to Pace3D
-    # Start Pace3D Simulation
-    # Transfer Pace3d simulation results into Grid object
+    for key, item in data_dict.items():
+        data_dict[key] = item / (1 + item)
+
+    actual_step['abaqus'].grid.set_node_values('porosity', data_dict)
